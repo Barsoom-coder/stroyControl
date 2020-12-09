7b993dbbd444fe4e9402573d9406933a5299974c
roman_kononov <romankononovdev@mail.ru>
2020-12-09 10:01:01 +0700

    added adaptability to section footer

--- app/js/app.js
+++ app/js/app.js
@@ -13,10 +13,37 @@ document.addEventListener('DOMContentLoaded', () => {
 		cssEase: 'linear',
 		arrows: false,
 		dots: true,
-    customPaging : function(slider, i) {
-        return '<img src="../img/dest/choice/slide-dot.png" /><img src="../img/dest/choice/slide-dot-active.png" />';
-    },
+		customPaging: function (slider, i) {
+			return '<img src="../img/dest/choice/slide-dot.png" /><img src="../img/dest/choice/slide-dot-active.png" />';
+		},
 	});
+
+	$('input:checkbox').change(function(){
+    if($(this).is(":checked")) {
+				$('.hamburger').addClass('hamburger--active');
+				$('.drawer-list').addClass('drawer-list--active');
+        $('body').addClass('fixedPosition');
+    } else {
+			$(this).prop("disabled", false);
+			$('.hamburger').removeClass('hamburger--active');
+				$('body').removeClass('fixedPosition');
+				$('.drawer-list').removeClass('drawer-list--active');
+    }
+});
+
+$('.scrolled').click(function(event){
+	event.preventDefault();
+	var id = $(this).attr('href'),
+	top = $(id).offset().top;
+	$('body,html').animate({scrollTop: top}, 1000);
+});
+
+$('.menu__link').click(function(e){
+		$("input:checkbox").prop("disabled", true);
+		$('.drawer-list').toggleClass('drawer-list--active');
+		$('body').toggleClass('fixedPosition');
+		$('.hamburger').removeClass('hamburger--active');
+});
 
 });
 
